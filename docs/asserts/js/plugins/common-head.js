e79593d26036b8d5736514f8310ab4b73670ac4e
bangwei.mo <bangwei.mo@lifesense.com>
2020-09-16 17:43:28 +0800

    添加文件

--- docs/asserts/js/plugins/common-head.js
+++
@@ -1,7 +0,0 @@
-proto._createSiteName = function() {
-    var linkEl = document.createElement('div');
-    linkEl.classList.add('logo');
-    linkEl.innerHTML =
-        "<a href=\"/\" class=\"docs-logo\"></a>";
-    return linkEl;
-};
